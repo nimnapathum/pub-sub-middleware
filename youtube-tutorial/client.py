2d65fd0cd1f96c7be00bc59808e8770b9585e955
Nimna Pathum <113501913+nimnapathum@users.noreply.github.com>
2025-07-03 16:59:25 +0530

    Add Youtube tutorial implementation of client-server communication

---
+++ youtube-tutorial/client.py
@@ -0,0 +1,22 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client_socket.send(send_length)
+    client_socket.send(message)
+    print(client_socket.recv(1024).decode(FORMAT))
+
+send("Hello, Server!")
